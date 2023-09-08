bf6c1f5f80a219b0cc900e138902acda9bc10627
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-09-08 14:31:25 -0400

    Compare with tracked, which has docs

--- app/app.ts
+++ app/app.ts
@@ -2,12 +2,14 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'test-cached/config/environment';
-import { cached } from '@glimmer/tracking';
+import { tracked, cached } from '@glimmer/tracking';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver;
+
+  @tracked foo2 = 2;
 
   @cached
   get foo() {
