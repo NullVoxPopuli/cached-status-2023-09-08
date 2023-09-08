8a876be6fa92f0a2460d3dcfb2433821dcc7a9ac
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-09-08 14:28:22 -0400

    test cached

--- app/app.ts
+++ app/app.ts
@@ -2,11 +2,17 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'test-cached/config/environment';
+import { cached } from '@glimmer/tracking';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver;
+
+  @cached
+  get foo() {
+    return 'hi';
+  }
 }
 
 loadInitializers(App, config.modulePrefix);
